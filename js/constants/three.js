daf67599d71986809974120b823e05f42d35c276
Anonimeishon <sebastianmperea@gmail.com>
2026-04-01 00:13:50 -0500

    Update GAME_CAMERA_ANIMATION to adjust camera position

--- js/constants/three.js
+++ js/constants/three.js
@@ -9,6 +9,6 @@ export const RESET_CAMERA_ANIMATION = {
 };
 export const GAME_CAMERA_ANIMATION = {
   duration: DEFAULT_CAMERA_ANIMATION_DURATION,
-  toPosition: new Vector3(0, 0.2, 1.3),
+  toPosition: new Vector3(0, 0.2, 1.5),
   toTarget: new Vector3(0, 0.2, 0),
 };

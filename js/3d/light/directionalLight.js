570038d4b6fb776b2739197f503effe4912942f7
Anonimeishon <sebastianmperea@gmail.com>
2026-04-06 22:50:41 -0500

    Update directional light intensity and position for improved lighting effects

--- js/3d/light/directionalLight.js
+++ js/3d/light/directionalLight.js
@@ -1,4 +1,4 @@
 import { DirectionalLight } from 'three';
 
-export const directionalLight = new DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 5, 5);
+export const directionalLight = new DirectionalLight(0xffffff, 2);
+directionalLight.position.set(4, 4, 4);

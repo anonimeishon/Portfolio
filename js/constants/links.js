e4e9e7f4f6bf260c2df09e07deb06346e316fc07
Anonimeishon <sebastianmperea@gmail.com>
2026-04-19 22:10:27 -0500

    fix: update GitHub link to point to the correct portfolio repository

--- js/constants/links.js
+++ js/constants/links.js
@@ -1,4 +1,4 @@
-export const LINK_GITHUB = 'https://github.com/anonimeishon';
+export const LINK_GITHUB = 'https://github.com/sMainero/Portfolio';
 export const LINK_LINKEDIN =
   'https://www.linkedin.com/in/sebastian-mainero-39571a1a5/';
 export const LINK_CONTACT = 'mailto:sebastianmperea@gmail.com';
